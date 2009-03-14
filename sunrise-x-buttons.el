952fd6cdcf31c83c1a4241cfc1c9fe304e6ec3de
José A. Romero L <escherdragon@gmail.com>
2009-03-14 13:28:42 +0000

    +Minor bugfix in sr-buttons-mc-keys-p
    git-svn-id: file:///opt/svn/emacs-tools/trunk@188 2ccc84cd-862f-49ff-a359-c231f00e8bd0

--- sunrise-x-buttons.el
+++ sunrise-x-buttons.el
@@ -218,7 +218,7 @@
 
 (defun sr-buttons-mc-keys-p ()
   "Determines whether mc style keybindings have been activated in Sunrise."
-  (< 1 (length (where-is-internal 'sr-goto-dir sr-mode-map))))
+  (< 2 (length (where-is-internal 'sr-goto-dir sr-mode-map))))
 
 (defun sr-buttons-maxtaglen ()
   "Calculates the length of the longest tag in sr-buttons-list."
