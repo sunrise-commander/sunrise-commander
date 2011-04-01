52cea2d9e597945fa4f05e3e37e92769411c633e
José A. Romero L <escherdragon@gmail.com>
2011-04-01 18:22:56 +0000

    +Minor enhancement: made sr-prev-subdir-other and sr-ti-prev-subdir prefixable
    git-svn-id: file:///opt/svn/emacs-tools/trunk@363 2ccc84cd-862f-49ff-a359-c231f00e8bd0

--- sunrise-commander.el
+++ sunrise-commander.el
@@ -2286,10 +2286,11 @@ automatically:
   (sr-mouse-change-window e)
   (sr-advertised-find-file))
 
-(defun sr-prev-subdir-other ()
+(defun sr-prev-subdir-other (&optional count)
   "Go to the previous subdirectory in the other pane."
-  (interactive)
-  (sr-in-other (sr-dired-prev-subdir)))
+  (interactive "P")
+  (let ((count (or count 1)))
+    (sr-in-other (sr-dired-prev-subdir count))))
 
 (defun sr-follow-file-other ()
   "Go to the same directory where the selected file is, but in the other pane."
@@ -3451,10 +3452,11 @@ or (c)ontents? ")
   (interactive)
   (sr-ti (dired-unmark-backward 1)))
 
-(defun sr-ti-prev-subdir ()
+(defun sr-ti-prev-subdir (&optional count)
   "Runs dired-prev-subdir on active pane from the terminal window."
-  (interactive)
-  (sr-ti (sr-dired-prev-subdir)))
+  (interactive "P")
+  (let ((count (or count 1)))
+    (sr-ti (sr-dired-prev-subdir count))))
 
 (defun sr-ti-unmark-all-marks ()
   "Removes all marks on active pane from the terminal window."
