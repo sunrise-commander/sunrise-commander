be9261da6eb1f3c331a0ac50ba6b9320b2c7e2f5
Lassi Kortela <lassi@lassi.io>
2019-08-11 11:56:06 +0300

    Use fill column 78 also for Markdown files

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ((emacs-lisp-mode . ((fill-column . 78)
                      (indent-tabs-mode . nil)))
+ (markdown-mode . ((fill-column . 78)))
  (nil . ((fill-column . 70)
          (default-justification . left)
          (require-final-newline . t))))
