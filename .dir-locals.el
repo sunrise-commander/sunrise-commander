4c89d18218fee4d3cd25ff30c9ea779f15f44eaa
Lassi Kortela <lassi@lassi.io>
2021-02-26 15:31:48 +0200

    Reformat dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,9 @@
-((emacs-lisp-mode . ((fill-column . 78)
-                     (indent-tabs-mode . nil)))
- (markdown-mode . ((fill-column . 78)))
- (nil . ((fill-column . 70)
-         (default-justification . left)
-         (require-final-newline . t))))
+((emacs-lisp-mode
+  (fill-column . 78)
+  (indent-tabs-mode . nil))
+ (markdown-mode
+  (fill-column . 78))
+ (nil
+  (fill-column . 70)
+  (default-justification . left)
+  (require-final-newline . t)))
